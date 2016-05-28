dcda24e50736560aba4097e3e04185484b5a5b89
Xavier Garcia Buils <xgbuils@gmail.com>
2016-05-28 18:59:34 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+var typeVerify = require('type-verify')
+
+function argumentsVerify (rules, argums, cb) {
+    rules = transformRules(rules)
+    var err = null
+    var ok = !rules.some(function (rule, index) {
+        if (typeVerify(rule, ['Object'])) {
+            var length = Math.min(argums.length, index + rule.times)
+            for (; index < length && !err; ++index) {
+                err = typeVerify(argums[index], rule.rule, errorHandlerCreator(index))
+            }
+        } else {
+            err = typeVerify(argums[index], rule, errorHandlerCreator(index))
+        }
+        return err
+    })
+    return cb ? cb(err, ok) : ok
+}
+
+function transformRules (rules) {
+    var result = []
+    rules.every(function (rule, index) {
+        ruleIsArray = typeVerify(rule, ['Array'])
+        if (ruleIsArray) {
+            result.push(rule)
+        } else if (typeVerify(rule, ['Number'])) {
+            result[index - 1] = {
+                rule: result[index - 1],
+                times: rule
+            }
+        }
+        return ruleIsArray
+    })
+    return result
+}
+
+function errorHandlerCreator (nth) {
+    return function (matches, value, expected, actual) {
+        return matches ? null : {
+            nth: nth,
+            value: value,
+            expected: expected,
+            actual: actual
+        }
+    }
+}
+
+module.exports = argumentsVerify
---
+++ test.js
@@ -0,0 +1,321 @@
+var expect = require('chai').expect
+var argumentsVerify = require('./index.js')
+var sinon = require('sinon')
+
+var OBJECT_STRING = new String('hello')
+var OBJECT_NUMBER = new Number(8)
+var OBJECT_BOOLEAN = new Boolean(false)
+var OBJECT = {}
+var PRIMITIVE_NUMBER = 6
+var PRIMITIVE_BOOLEAN = true
+var REGEXP = /abc/
+var ARRAY = [1, 2, 3]
+var GENERIC_ERROR = new Error('muahaha')
+var FUNCTION = function () {}
+
+function fnTestCreator(rules, cb) {
+    return function () {
+        return argumentsVerify(rules, arguments, cb)
+    }
+}
+
+describe('argumentsVerify', function () {
+    describe('without callback', function () {
+        it('returns true if all of arguments are valid', function () {
+            var RULES = [
+                [String],
+                ['Number'],
+                ['Array', 'RegExp']
+            ]
+            var foo = fnTestCreator(RULES)
+            expect(foo(OBJECT_STRING, PRIMITIVE_NUMBER, ARRAY)).to.be.equal(true)
+        })
+
+        it('arguments no specified in rules are always valid', function () {
+            var RULES = [
+                ['RegExp'],
+                [],
+                [Number]
+            ]
+            var foo = fnTestCreator(RULES)
+            expect(foo(REGEXP, ARRAY, OBJECT_NUMBER, ARRAY, OBJECT_STRING)).to.be.equal(true)
+        })
+
+        it('returns false when first argument is invalid', function () {
+            var RULES = [
+                ['String'],
+                ['Object'],
+                []
+            ]
+            var foo = fnTestCreator(RULES)
+            expect(foo(ARRAY, OBJECT, PRIMITIVE_NUMBER)).to.be.equal(false)
+        })
+
+        it('returns false when third argument is invalid', function () {
+            var RULES = [
+                ['Boolean'],
+                [Boolean],
+                ['Null']
+            ]
+            var foo = fnTestCreator(RULES)
+            expect(foo(OBJECT_BOOLEAN, OBJECT_BOOLEAN)).to.be.equal(false)
+        })
+
+        it('returns false when more than one argument is invalid', function () {
+            var RULES = [
+                ['Boolean'],
+                [Boolean],
+                ['Null']
+            ]
+            var foo = fnTestCreator(RULES)
+            expect(foo(OBJECT_BOOLEAN, PRIMITIVE_BOOLEAN)).to.be.equal(false)
+        })
+
+        describe('optional arguments validation', function () {
+            describe('when last item of rules is a number `n` and given the previous rule', function () {
+                describe('the first `n` optional arguments passed, are verified with the last rule', function () {
+                    it('valid arguments (not putting optional arguments)', function () {
+                        var n = 5
+                        var RULES = [
+                            ['Function'],
+                            ['Number'],
+                            n
+                        ]
+                        var foo = fnTestCreator(RULES)
+                        expect(foo(FUNCTION)).to.be.equal(true)
+                    })
+
+                    it('valid arguments (not putting all of optional arguments to verify)', function () {
+                        var n = 3
+                        var RULES = [
+                            ['Function'],
+                            ['Number'],
+                            n
+                        ]
+                        var foo = fnTestCreator(RULES)
+                        expect(foo(FUNCTION, PRIMITIVE_NUMBER, OBJECT_NUMBER)).to.be.equal(true)
+                    })
+
+                    it('valid arguments (putting exactly the same arguments as `n`)', function () {
+                        var n = 2
+                        var RULES = [
+                            ['Function'],
+                            ['Number'],
+                            n
+                        ]
+                        var foo = fnTestCreator(RULES)
+                        expect(foo(FUNCTION, PRIMITIVE_NUMBER, OBJECT_NUMBER)).to.be.equal(true)
+                    })
+
+                    it('valid arguments (putting more arguments than `n`)', function () {
+                        var n = 2
+                        var RULES = [
+                            ['Function'],
+                            ['Number'],
+                            n
+                        ]
+                        var foo = fnTestCreator(RULES)
+                        expect(foo(FUNCTION, PRIMITIVE_NUMBER, OBJECT_NUMBER, ARRAY, FUNCTION)).to.be.equal(true)
+                    })
+
+                    it('invalid arguments (not putting all of optional arguments to verify)', function () {
+                        var n = 3
+                        var RULES = [
+                            ['Function'],
+                            ['Number'],
+                            n
+                        ]
+                        var foo = fnTestCreator(RULES)
+                        expect(foo(FUNCTION, PRIMITIVE_NUMBER, REGEXP)).to.be.equal(false)
+                    })
+
+                    it('invalid arguments (putting exactly the same arguments as `n`)', function () {
+                        var n = 2
+                        var RULES = [
+                            ['Function'],
+                            ['Number'],
+                            n
+                        ]
+                        var foo = fnTestCreator(RULES)
+                        expect(foo(FUNCTION, OBJECT, OBJECT_NUMBER)).to.be.equal(false)
+                    })
+
+                    it('invalid arguments (putting more arguments than `n`)', function () {
+                        var n = 2
+                        var RULES = [
+                            ['Function'],
+                            ['Number'],
+                            n
+                        ]
+                        var foo = fnTestCreator(RULES)
+                        expect(foo(FUNCTION, PRIMITIVE_NUMBER, REGEXP, ARRAY, FUNCTION)).to.be.equal(false)
+                    })
+                })
+            })
+        })
+    })
+
+    describe('with callback', function () {
+        var cb
+        beforeEach(function () {
+            cb = sinon.spy()
+        })
+
+        it('callback is called once when arguments are valid', function () {
+            var RULES = [
+                ['Boolean'],
+                ['Undefined']
+            ]
+            var foo = fnTestCreator(RULES, cb)
+            foo(PRIMITIVE_BOOLEAN, undefined)
+            expect(cb.calledOnce).to.be.equal(true)
+        })
+
+        it('callback is called once when some argument is invalid', function () {
+            var RULES = [
+                ['Number'],
+                [TypeError],
+            ]
+            var foo = fnTestCreator(RULES, cb)
+            foo(PRIMITIVE_NUMBER, GENERIC_ERROR)
+            expect(cb.calledOnce).to.be.equal(true)
+        })
+
+        describe('error argument (first argument)', function () {
+            it('is null if all of arguments are valid', function () {
+                var RULES = [
+                    [String],
+                    ['Number'],
+                    ['Array', 'RegExp']
+                ]
+                var foo = fnTestCreator(RULES, cb)
+                foo(OBJECT_STRING, PRIMITIVE_NUMBER, ARRAY)
+                expect(cb.args[0][0]).to.be.equal(null)
+            })
+
+            it('is null when are passed arguments no specified in rules', function () {
+                var RULES = [
+                    ['RegExp'],
+                    [],
+                    [Number]
+                ]
+                var foo = fnTestCreator(RULES, cb)
+                foo(REGEXP, ARRAY, OBJECT_NUMBER, ARRAY, OBJECT_STRING)
+                expect(cb.args[0][0]).to.be.equal(null)
+            })
+
+            it('error object is passed when first argument is invalid', function () {
+                var RULES = [
+                    ['String', Number],
+                    ['Object'],
+                    []
+                ]
+                var foo = fnTestCreator(RULES, cb)
+                foo(ARRAY, OBJECT, PRIMITIVE_NUMBER)
+                expect(cb.args[0][0]).to.be.deep.equal({
+                    nth: 0,
+                    value: ARRAY,
+                    actual: {
+                        type: 'Array',
+                        instance: 'Array'
+                    },
+                    expected: {
+                        type: ['String'],
+                        instance: ['Number']
+                    }
+                })
+            })
+
+            it('error object is passed when third argument is invalid', function () {
+                var RULES = [
+                    ['Boolean'],
+                    [Boolean],
+                    ['Null']
+                ]
+                var foo = fnTestCreator(RULES, cb)
+                foo(OBJECT_BOOLEAN, OBJECT_BOOLEAN)
+                expect(cb.args[0][0]).to.be.deep.equal({
+                    nth: 2,
+                    value: undefined,
+                    actual: {
+                        type: 'Undefined'
+                    },
+                    expected: {
+                        type: ['Null'],
+                        instance: []
+                    }
+                })
+            })
+
+            it('error object with the first argument invalid is passed when more than one argument is invalid', function () {
+                var RULES = [
+                    ['Boolean'],
+                    [Boolean],
+                    ['Null']
+                ]
+                var foo = fnTestCreator(RULES, cb)
+                foo(OBJECT_BOOLEAN, PRIMITIVE_BOOLEAN)
+                expect(cb.args[0][0]).to.be.deep.equal({
+                    nth: 1,
+                    value: PRIMITIVE_BOOLEAN,
+                    actual: {
+                        instance: 'Boolean'
+                    },
+                    expected: {
+                        type: [],
+                        instance: ['Boolean']
+                    }
+                })
+            })
+        })
+
+        describe('match argument (second argument)', function () {
+            it('is true when arguments are valid', function () {
+                var RULES = [
+                    ['Boolean'],
+                    ['Undefined']
+                ]
+                var foo = fnTestCreator(RULES, cb)
+                foo(PRIMITIVE_BOOLEAN, undefined)
+                expect(cb.args[0][1]).to.be.equal(true)
+            })
+
+            it('is false when some argument is invalid', function () {
+                var RULES = [
+                    ['Number'],
+                    [TypeError],
+                ]
+                var foo = fnTestCreator(RULES, cb)
+                foo(PRIMITIVE_NUMBER, GENERIC_ERROR)
+                expect(cb.args[0][1]).to.be.equal(false)
+            })
+        })
+        describe('optional arguments validation', function () {
+            describe('when last item of rules is a number `n` and given the previous rule', function () {
+                describe('the first `n` optional arguments passed, are verified with the last rule', function () {
+                    it('callback is called once when arguments are valid', function () {
+                        var RULES = [
+                            ['Boolean'],
+                            ['Boolean'],
+                            1
+                        ]
+                        var foo = fnTestCreator(RULES, cb)
+                        foo(PRIMITIVE_BOOLEAN)
+                        expect(cb.calledOnce).to.be.equal(true)
+                    })
+
+                    it('callback is called once when some argument is invalid', function () {
+                        var RULES = [
+                            ['Number'],
+                            [Error],
+                            4
+                        ]
+                        var foo = fnTestCreator(RULES, cb)
+                        foo(PRIMITIVE_NUMBER, GENERIC_ERROR, GENERIC_ERROR, GENERIC_ERROR, PRIMITIVE_NUMBER)
+                        expect(cb.calledOnce).to.be.equal(true)
+                    })
+                })
+            })
+        })
+    })
+})
